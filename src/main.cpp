2f4612e9cd4ee942d57a18d3e73a248b9f4bae80
Bálint Fodor <balint.fodor@gmail.com>
2018-07-14 17:04:36 +0200

    using breaks, trying accelerate when cte is small

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  pid.Init(0.2, 0.000025, 0.01);
+  pid.Init(0.15, 0.000025, 0.075);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -46,26 +46,48 @@ int main()
         auto j = json::parse(s);
         std::string event = j[0].get<std::string>();
         if (event == "telemetry") {
-          // j[1] is the data JSON object
-          double cte = std::stod(j[1]["cte"].get<std::string>());
-          double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value = 0.0;
-          
-          pid.UpdateError(cte);
-          double u = -pid.TotalError();
-          u = fmax(-1.0, fmin(1.0, u));
-          steer_value = u;
+            // j[1] is the data JSON object
+            double cte = std::stod(j[1]["cte"].get<std::string>());
+            double speed = std::stod(j[1]["speed"].get<std::string>());
+            double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+            double steer_value = 0.0;
 
-          // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+            pid.Kp = 0.15;
+            if (speed > 15) {
+                if (speed > 30) {
+                    pid.Kp = 0.0375;
+                } else {
+                    pid.Kp = 0.075;
+                }
+            }
 
-          json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.1;
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+            pid.UpdateError(cte);
+            double u = -pid.TotalError();
+            u = fmax(-1.0, fmin(1.0, u));
+            steer_value = u;
+
+            double throttle = 0.05;
+            if (fabs(cte) > 1.3 && pid.d_error > 0.005 && speed > 10) {
+                throttle = -0.01;
+            }
+
+            if (fabs(cte) < 0.6) {
+                if (fabs(cte) < 0.2) {
+                    throttle = 0.7;
+                } else {
+                    throttle = 0.1;
+                }
+            }
+
+            // DEBUG
+            std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+
+            json msgJson;
+            msgJson["steering_angle"] = steer_value;
+            msgJson["throttle"] = throttle;
+            auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+            std::cout << msg << std::endl;
+            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
         // Manual driving
